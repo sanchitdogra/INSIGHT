d08ec496557c070bb8a40e4ee578ecaacd10f49d
Sanchit Dogra <69036310+sanchitdogra@users.noreply.github.com>
2021-02-10 20:34:45 +0530

    Add files via upload

--- ap.js
+++ ap.js
@@ -64,5 +64,22 @@ ref.on("value", function(snapshot)
   //Reference contactInfo collections
   let contactInfo = firebase.database().ref("feedback/"+name);
 
+// //function to save file
+// function uploadFile(){
+      
+//   // Created a Storage Reference with root dir
+//   var storageRef = firebase.storage().ref();
+//   // Get the file from DOM
+//   var file = document.getElementById("files").files[0];
+  
+//   //dynamically set reference to the file name
+//   var thisRef = storageRef.child(file.name);
+
+//   //put request upload file to firebase storage
+//   thisRef.put(file).then(function(snapshot) {
+//      alert("File Uploaded")
+//      console.log('Uploaded a blob or file!');
+//   });
+// }
 
 
